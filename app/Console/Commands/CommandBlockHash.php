cf63b7fe65fe370ed73cf5d63f279de9be2effba
Argha Nilanjon Nondi <pcic095@gmail.com>
2023-09-09 23:52:50 +0600

    blockchain is added in audit

--- app/Console/Commands/CommandBlockHash.php
+++ app/Console/Commands/CommandBlockHash.php
@@ -21,60 +21,74 @@ class CommandBlockHash extends Command
   */
   protected $description = 'Command description';
 
+
+  public function make_hash() {
+
+    $difficulty = 4; // difficulty level of hash
+
+    // get a record which previous_hash,current_hash and nonce is not set yet
+    // the record will be first old record
+    $currentRecord = BlockChainAudit::whereNull('current_hash')
+                                    ->whereNull('previous_hash')
+                                    ->whereNull("nonce")
+                                    ->select("id", "user_id", "user_type",
+                                              "event", "auditable_id", "auditable_type",
+                                              "old_values", "new_values", "ip_address", "url", "user_agent", "tags", 'created_at')
+                                      ->orderBy('created_at', 'asc')
+                                      ->first();
+    
+    if($currentRecord==null){
+       $this->info("Every record is hashed \n");
+       return 0;
+    }
+
+    // get the previous record of $currentRecord 
+    $previousRecord = BlockChainAudit::where('created_at', '<', $currentRecord->created_at)
+                                      ->select("current_hash")
+                                      ->orderBy('created_at', 'desc')
+                                      ->first();
+
+     // generate nonce from 0 to N 
+     // if the nonce is appropiate then $hashed_string will start with N number of "0"
+     // here N is $difficulty
+    for ($nonce = 0;;$nonce = $nonce+1) {
+      
+      // prepare data with nonce to crearte hash
+      $data = json_encode([
+        "data" => $currentRecord,
+        "previous" => $previousRecord->current_hash,
+        "nonce" => $nonce
+      ]);
+
+      // hash the data
+      $hashed_string = hash('sha256', $data);
+
+      // get first N character from $hashed_string according to $difficulty
+      $first_four = substr($hashed_string, 0, $difficulty);
+
+      // compare with hash
+      if ($first_four === str_repeat("0", $difficulty)) {
+        echo $hashed_string."\n";
+        
+        // entry the data to Audits model
+        BlockChainAudit::where("id", $currentRecord->id)
+        ->update([
+          "previous_hash" => $previousRecord->current_hash,
+          "current_hash" => $hashed_string,
+          "nonce" => $nonce,
+        ]);
+        break;
+      }
+    }
+
+}
+
   /**
   * Execute the console command.
   */
   public function handle() {
-    
-    $difficulty=4;
-    
-    $currentRecord = BlockChainAudit::whereNull('current_hash')
-                              ->whereNull('previous_hash')
-                              ->whereNull("nonce")
-                              ->select("id", "user_id", "user_type",
-                                        "event", "auditable_id", "auditable_type",
-                                        "old_values", "new_values", "ip_address", "url", "user_agent", "tags", 
-                                        \DB::raw('DATE_FORMAT(created_at, "%Y-%m-%d %H:%i:%s") as created_at'))
-                              ->orderBy('created_at', 'asc')
-                              ->first();
-
-    $previousRecord = BlockChainAudit::where('created_at', '<', $currentRecord->created_at)
-                                    ->select("current_hash")
-                                    ->orderBy('created_at', 'desc')
-                                    ->first();
-
-    echo $currentRecord->id;
-    //echo $previousRecord;
-    
-    for($nonce=0;;$nonce=$nonce+1){
-      
-      $data=json_encode([
-        "data"=>$currentRecord,
-        "previous"=>$previousRecord->current_hash,
-        "nonce"=>$nonce
-        ]);
-        
-        echo $data;
-        break;
-      $hashed_string=hash('sha256', $data);
-        /*
-      $first_four=substr($hashed_string,0,$difficulty);
-      
-      if($first_four===str_repeat("0",$difficulty)){
-        echo "hi";
-        BlockChainAudit::where("id",$currentRecord->id)
-        ->update([
-          "previous_hash"=>$previousRecord->current_hash,
-          "current_hash"=>$hashed_string,
-          "nonce"=>$nonce,
-          ]);
-        break;
-      }
-      
-      */
-    
-    }
-    
-    
+    $this->make_hash();
   }
+
+
 }
